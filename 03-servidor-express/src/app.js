e0cb049bf6bdc67529dc544a8cab987d445f49e6
johnnyargote123 <alexco12@hotmail.es>
2023-03-10 18:39:15 -0500

    correciones

--- 03-servidor-express/src/app.js
+++ 03-servidor-express/src/app.js
@@ -5,30 +5,49 @@ const manager = new ProductManager()
 
 
 
-    let consulta = await manager.getProduct()
+  
 
 
 
-    app.get("/products", (req,res) => {
+    app.get("/products", async (req,res) => {
+        try {
+
+        let consulta = await manager.getProduct()
 
         const limit = req.query.limit
 
         if(!limit){
             res.send(consulta)
         }
+        else{
+            const limitNum = parseInt(limit)
+            const limitDataCosulta = consulta.slice(0, limitNum)
+            res.send(limitDataCosulta)
+        }
 
-        const limitNum = parseInt(limit)
-        const limitDataCosulta = consulta.slice(0, limitNum)
-        res.send(limitDataCosulta)
+        
+        } catch (error) {
+            console.log(error)
+        }
+    
     })
 
 
-    app.get("/products/:pid", (req, res) => {
+    app.get("/products/:pid", async (req, res) => {
+        try {
+        let consulta = await manager.getProduct()
+
         const idProducto = req.params.pid
         const producto = consulta.find((u) => u.id === idProducto)
-        if(!producto) return res.send({ error: "usuario no encontrado"})
+        if(!producto) return res.send({ error: "producto no encontrado"})
         res.send(producto)
     
+        
+
+    } catch (error) {
+        console.log(error)
+    }
+
     }) 
 
     app.listen(8082, () => {
