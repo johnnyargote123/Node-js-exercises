4374c75582f5d2898e36e8304d0135c216a4c878
johnnyargote123 <alexco12@hotmail.es>
2023-07-10 11:05:13 -0500

    documentar api

---
+++ 15-documentar-api/src/middlewares/rol.js
@@ -0,0 +1,9 @@
+function updateUserRoleMiddleware (req, res, next) {
+    const { email } = req.body;
+    if (email !== 'adminCoder@coder.com') {
+      req.body.rol = 'USER';
+    }
+    next();
+  };
+  
+  export default updateUserRoleMiddleware;
