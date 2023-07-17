88ac29c5ae5c4a1cfd6050a713aabbae4648ab12
johnnyargote123 <alexco12@hotmail.es>
2023-07-16 20:29:12 -0500

    desafio 16 en proceso

---
+++ 16-modulos-de-testing-proyecto/src/dao/repositories/session.repository.js
@@ -0,0 +1,109 @@
+import { sessionMongo } from "../mongo/session.mongo.js";
+
+class SessionRepository {
+
+
+
+  async loginUser(email, password) {
+    try {
+      const user = await sessionMongo.findOneByEmail(email);
+
+      if (!user || user.password !== password) {
+        throw new Error("Incorrect credentials");
+      }
+
+      await sessionMongo.updateUserByEmail(email, { $set: { rol: user.rol } });
+
+      const result = {
+        first_name: user.first_name, 
+        last_name: user.last_name,
+        email: user.email,
+        age: user.age,
+        rol: user.rol,
+      };
+      this.currentUser = result
+      return result
+    } catch (error) {
+      console.log(error);
+      throw new Error("Internal server error");
+    }
+  }
+
+  async resetPassUser (email,password,token){
+    const user = await sessionMongo.findOneByEmail(email);
+    try {
+      console.log(password,user.password)
+      if (password == user.password) {
+        throw new Error("You can not change password with the last password");
+      }
+      if (password !== user.password) {
+        await sessionMongo.updateUserByEmail(email, { $set: { password: password } });
+        return user.password        
+      }
+
+    } catch (error) {
+      console.log(error);
+      throw new Error("Internal server error");
+    }
+  }
+
+  async logoutUser(data) {
+    try {
+      data.user = null;
+      data.loggedIn = false;
+      return "Logout successful";
+    } catch (error) {
+      console.log(error);
+      throw new Error("An error occurred while logging out.");
+    }
+  }
+
+  async registerUser(first_name, last_name, email, age, password) {
+    try {
+      const userExists = await sessionMongo.findOneByEmail(email);
+      if (userExists) {
+        throw new Error("User already exists");
+      }
+
+      const user = {
+        first_name,
+        last_name,
+        email,
+        age,
+        password,
+        rol: '',
+      };
+      await sessionMongo.createUser(user);
+      return { message: "User registered" };
+    } catch (error) {
+      console.log(error);
+      throw new Error("Internal server error");
+    }
+  }
+
+
+
+  async forgotPasswordUser(email) {
+    try {
+      const user = await sessionMongo.findOneByEmail(email);
+
+      if (!user) {
+        throw new Error("User not found");
+      }
+      return { message: "User find" };
+    } catch (error) {
+      console.log(error);
+      throw new Error("Internal server error");
+    }
+  }
+
+  githubAuth() {
+    // Implement GitHub authentication logic here
+  }
+
+  githubCallback(req) {
+    return req.user;
+  }
+}
+
+export const sessionRepository = new SessionRepository();
---
+++ 16-modulos-de-testing-proyecto/src/services/products.service.js
@@ -0,0 +1,98 @@
+import { productsRepository } from "../dao/repositories/products.repository.js";
+
+class ProductService {
+  async getAllProducts(limit, page, category, status, sort) {
+    try {
+      const products = await productsRepository.getAllProducts(
+        limit,
+        page,
+        category,
+        status,
+        sort
+      );
+      return products;
+    } catch (error) {
+      throw new Error("There are no products registered");
+    }
+  }
+
+  async getProductById(id) {
+    try {
+      const product = await productsRepository.getProductById(id);
+      if (product) {
+        return product;
+      } else {
+        throw new Error(`Not found this product ID: ${id}`);
+      }
+    } catch (error) {
+      throw new Error(`Not found this product ID: ${id}`);
+    }
+  }
+
+  async createProduct(product, currentUser, currentRol) {
+
+    try {
+      if(product.owner && currentRol == "PREMIUM"){
+          product.owner = currentUser       
+      }
+      const createdProduct = await productsRepository.createProduct(product);
+      return createdProduct;
+    } catch (error) {
+      throw new Error("Error creating product");
+    }
+  }
+
+  async updateProduct(id, product) {
+    try {
+      const updatedProduct = await productsRepository.updateProduct(
+        id,
+        product
+      );
+      return updatedProduct;
+    } catch (error) {
+      throw new Error("Incomplete values");
+    }
+  }
+
+  async deleteProduct(id, currentUser, currentRol) {
+    try {
+      const deletedProduct = await productsRepository.deleteProduct(id,currentUser, currentRol);
+      return deletedProduct;
+    } catch (error) {
+      throw new Error("Product does not exist");
+    }
+  }
+
+
+  async getAvailableStock(productId) {
+
+    try {
+      const product = await productsRepository.getProductById(productId);
+      if (product) {
+        return product.payload.stock;
+      } else {
+        throw new Error(`Product with ID ${productId} not found`);
+      }
+    } catch (error) {
+      throw new Error(`Error retrieving available stock for product with ID ${productId}: ${error.message}`);
+    }
+  }
+
+  async updateStock(productId, newStock) {
+    try {
+      const updatedProduct = await productsRepository.updateStock(productId, newStock);
+      if (!updatedProduct) {
+        throw new Error("Product not found");
+      }
+
+      return updatedProduct;
+    } catch (error) {
+      throw new Error(`Error updating product stock: ${error.message}`);
+    }
+  }
+
+}
+
+
+
+export const productService = new ProductService();
